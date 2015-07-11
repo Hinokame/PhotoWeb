663dd5f50eae1eca9d1a3071227e7122ee93a57a
Andrius Alimas <andriusjavait@gmail.com>
2015-07-11 03:25:31 +0100

    Running embeded Server fix

--- src/main/java/com/andrew/server/StartServer.java
+++ src/main/java/com/andrew/server/StartServer.java
@@ -19,7 +19,7 @@ public class StartServer{
         //Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
-            webPort = "8090";
+            webPort = "8090"; // port
         }
 
         tomcat.setPort(Integer.valueOf(webPort));
